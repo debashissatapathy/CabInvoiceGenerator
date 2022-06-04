e0ba4b7ecc2830567d678d733027025e12dca2e0
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-06-04 10:42:19 +0530

    [Debashis] Amount for cab ride

---
+++ CabInvoiceGenerator/CabInvoiceGenerator/CabInvoice.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CabInvoiceGenerator
+{
+    public class InvoiceGenerator    {
+        public double Distance;
+        public int Time;
+        public double[] Ride;
+        public double TotalFare;
+        public double AvgFare;
+        public int NoOfRide;
+        private readonly double CostPerKilometer;
+        private readonly int CostPerTime;
+        private readonly double MinimumFare;
+
+        public InvoiceGenerator()
+        {
+            CostPerKilometer = 10;
+            CostPerTime = 1;
+            MinimumFare = 5;
+        }
+        public double CalculateFare(double Distance, int Time)
+        {
+            TotalFare = 0;
+            TotalFare = (Distance * CostPerKilometer) + (CostPerTime * Time);
+            return TotalFare;
+        }
+        public double CalculateFare(MultipleRides[] ride)
+        {
+            double totalfare = 0;
+            foreach (MultipleRides rides in ride)
+            {
+                Console.WriteLine(totalfare);
+                totalfare = totalfare + CalculateFare(rides.distance, rides.time);
+                Console.WriteLine(totalfare);
+            }
+            
+            return totalfare;
+        }
+    }
+}
---
+++ CabInvoiceGenerator/CabInvoiceGenerator/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CabInvoiceGenerator;
+bool run = true;
+while (run)
+{
+    Console.WriteLine("==============Cab Invoice==============");
+    Console.WriteLine("Select option: \n1.Calculate fare \n2.Multiple Rides");
+    int op = Convert.ToInt16(Console.ReadLine());
+    switch (op)
+    {
+        case 1:
+            //Step 1-CalculateFare-NormalRide
+            InvoiceGenerator invoice = new InvoiceGenerator();
+            double fare = invoice.CalculateFare(3.0, 5);
+            Console.WriteLine("Calculate Fare: {0}", fare);
+            break;
+        case 2:
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+            MultipleRides[] rides = { new MultipleRides(3.0, 5), new MultipleRides(5, 10) };
+            double totalfare = invoiceGenerator.CalculateFare(rides);            
+            System.Console.WriteLine("Toatal Fare:" + totalfare);
+            break;
+    }
+}
+
+
